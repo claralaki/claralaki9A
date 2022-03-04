ac6a4cf0fe9a52111728cab12942e9a366253593
claralaki <99336633+claralaki@users.noreply.github.com>
2022-03-04 10:40:50 -0300

    Update codigo.ino

--- Aula 5/codigo.ino
+++ Aula 5/codigo.ino
@@ -23,5 +23,36 @@ Serial.begin(9600);
  {
   var = analogRead(pot);
   Serial.println(var);
+   
+   if(var > 50){
+     digitalWrite(led1, HIGH);
+   }else{
+     digitalWrite(led1, LOW);
+   }
+   
+   if(var > 200){
+     digitalWrite(led2, HIGH);
+   }else{
+     digitalWrite(led2, LOW);
+   }
+      
+   if(var > 400){
+     digitalWrite(led3, HIGH);
+   }else{
+     digitalWrite(led3, LOW);
+   }
+      
+   if(var > 600){
+     digitalWrite(led4, HIGH);
+   }else{
+     digitalWrite(led4, LOW);
+   }
+      
+   if(var > 900){
+     digitalWrite(led5, HIGH);
+   }else{
+     digitalWrite(led5, LOW);
+   }
+  
   delay(200);
  }
