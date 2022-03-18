a6d66aa38d03ee422d5a375444fdab385253e7a3
claralaki <99336633+claralaki@users.noreply.github.com>
2022-03-18 10:43:11 -0300

    Update codigo9.ino

--- exercicios/ex1/ex2/ex3/ex4/ex5/ex6/ex7/ex8/ex9/codigo9.ino
+++ exercicios/ex1/ex2/ex3/ex4/ex5/ex6/ex7/ex8/ex9/codigo9.ino
@@ -3,6 +3,11 @@ const int led2 = 3;
 const int led3 = 4;
 const int led4 = 5;
 const int led5 = 6;
+const int led6 = 7;
+const int led7 = 8;
+const int led8 = 9;
+const int led9 = 10;
+const int led10 = 11;
 
 const int pot = 0;
 
@@ -15,6 +20,11 @@ int var = 0;
    pinMode(led3, OUTPUT);
    pinMode(led4, OUTPUT);
    pinMode(led5, OUTPUT);
+   pinMode(led6, OUTPUT);
+   pinMode(led7, OUTPUT);
+   pinMode(led8, OUTPUT);
+   pinMode(led9, OUTPUT);
+   pinMode(led10, OUTPUT);
    
 Serial.begin(9600);
  }
@@ -24,7 +34,7 @@ Serial.begin(9600);
   var = analogRead(pot);
   Serial.println(var);
    
-   if(var > 50){
+   if(var > 100){
      digitalWrite(led1, HIGH);
    }else{
      digitalWrite(led1, LOW);
@@ -36,23 +46,49 @@ Serial.begin(9600);
      digitalWrite(led2, LOW);
    }
       
-   if(var > 400){
+   if(var > 300){
      digitalWrite(led3, HIGH);
    }else{
      digitalWrite(led3, LOW);
    }
       
-   if(var > 600){
+   if(var > 400){
      digitalWrite(led4, HIGH);
    }else{
      digitalWrite(led4, LOW);
    }
       
-   if(var > 900){
+   if(var > 500){
      digitalWrite(led5, HIGH);
    }else{
      digitalWrite(led5, LOW);
    }
+   if(var > 600){
+     digitalWrite(led6, HIGH);
+   }else{
+     digitalWrite(led6, LOW);
+   }
+  if(var > 700){
+     digitalWrite(led7, HIGH);
+   }else{
+     digitalWrite(led7, LOW);
+   }
+  if(var > 800){
+     digitalWrite(led8, HIGH);
+   }else{
+     digitalWrite(led8, LOW);
+   }
+  if(var > 900){
+     digitalWrite(led9, HIGH);
+   }else{
+     digitalWrite(led9, LOW);
+   }
+  if(var > 1000){
+     digitalWrite(led10, HIGH);
+   }else{
+     digitalWrite(led10, LOW);
+   }
+  
   
   delay(200);
  }
